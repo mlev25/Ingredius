0b379401e57dc185a258a5a426c7f2f2cf095bd9
Mészáros Levente <mlevente25@mailbox.unideb.hu>
2025-11-27 17:09:24 +0200

    initial commit: project initialization

---
+++ src/test/java/hu/unideb/inf/ingredius/IngrediusApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.ingredius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngrediusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
